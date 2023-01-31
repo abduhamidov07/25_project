498affb3a46aad9e8b292c7bb8632953c9d041ed
Sardor-Abduhamidov007 <sardorabduhamidov007@gmail.com>
2023-01-31 23:11:31 +0500

    initial commit

---
+++ 25 timer/script.js
@@ -0,0 +1,56 @@
+const timerElem = document.querySelector(".time");
+const startBtn = document.querySelector(".start");
+const pauseBtn = document.querySelector(".pause");
+const resetBtn = document.querySelector(".reset");
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let t = null;
+
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resetBtn.addEventListener("click", resetTimer);
+
+function startTimer() {
+  if (t !== null) {
+    clearInterval(t);
+  }
+  t = setInterval(displayTime, 10);
+}
+
+function pauseTimer() {
+  clearInterval(t);
+}
+
+function resetTimer() {
+  clearInterval(t);
+  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+  timerElem.innerHTML = "00 : 00 : 00 : 000";
+}
+
+// Display Timer
+function displayTime() {
+  milliseconds += 10;
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hours++;
+      }
+    }
+  }
+
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let ms =
+    milliseconds < 10
+      ? "00" + milliseconds
+      : milliseconds < 100
+      ? "0" + milliseconds
+      : milliseconds;
+
+  timerElem.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
